e666bbf13faf6df1c2159659271da1792d3255d3
RALC365 <alejandro.carcamo@unitec.edu>
2017-11-10 12:56:16 -0600

    prueba2

--- src/examen1_richardson_lainez/Examen1_Richardson_Lainez.java
+++
@@ -1,7 +0,0 @@
-package examen1_richardson_lainez;
-public class Examen1_Richardson_Lainez {
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
